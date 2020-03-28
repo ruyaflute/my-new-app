0bc41de870ade1d49761d46bc50393863f222601
nancyruya <nancyruya@gmail.com>
2020-03-28 15:56:41 -0400

    small exercise

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText] = useState('Text')
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! working</Text>
+    <View style={styles.container}> 
+      <Text>{outputText}</Text>
+      <Button title="Change Text" onPress={() => setOutputText('The text changed!')}/>
     </View>
   );
 }
