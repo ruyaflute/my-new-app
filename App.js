715106d794fcb0b7a4ba86988809d47189b88c3d
nancyruya <nancyruya@gmail.com>
2020-03-28 19:42:36 -0400

    Care Plan add goal

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Text')
   return (
-    <View style={styles.container}> 
-      <Text>{outputText}</Text>
-      <Button title="Change Text" onPress={() => setOutputText('The text changed!')}/>
+    <View style={{ padding: 50 }}> 
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+        <TextInput
+        placeholder="Care Plan Goal"
+        style={{ width: '80%', borderColor: 'black', borderWidth: 1 , padding: 10 }}
+        />
+        <Button title="ADD" />
+      </View>
+      <View>
+      </View>
+      
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
