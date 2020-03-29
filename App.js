74264be45821a72181fd10f41c48305aa3e4c68a
nancyruya <nancyruya@gmail.com>
2020-03-29 13:06:30 -0400

    const styles

--- App.js
+++ App.js
@@ -2,37 +2,44 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  return (  
-    <View style={{ padding: 50 }}> 
-    <Text style={{
-      fontSize: 30,
-      textAlign:'center',
-      color:'black',
-      fontWeight:'bold'
-      }}> Care Plan </Text>
-      <View style={{ 
-        flexDirection: 'row', 
-        justifyContent: 'space-between', 
-        alignItems: 'center' 
-        }}>
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.carePlan}> Care Plan </Text>
+      <View style={styles.inputContainer}>
         <TextInput
-        placeholder="Care Plan Goal"
-        style={{ 
-          width: '80%', 
-          borderColor: 'black', 
-          borderWidth: 1 , 
-          padding: 10 }}
+          placeholder="Care Plan Goal"
+          style={styles.input}
         />
         <Button title="ADD" />
       </View>
       <View>
       </View>
-      
+
     </View>
 
   );
 }
 
 const styles = StyleSheet.create({
-
+  screen: {
+    padding: 50,
+  },
+  carePlan: {
+    fontSize: 30,
+    textAlign: 'center',
+    color: 'black',
+    fontWeight: 'bold',
+    padding: 20
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  }
 });
