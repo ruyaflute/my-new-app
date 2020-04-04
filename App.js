2bfb973dc9845040e26b84e8a10c26cdfd0f98d2
nancyruya <nancyruya@gmail.com>
2020-04-04 11:25:14 -0400

    FlatList for loading long list, VirtualizedLists should never be nested inside plain ScrollViews with the same orientation - use another VirtualizedList-backed container instead.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
@@ -9,11 +9,11 @@ export default function App() {
     setEnteredGoal(enteredText);
   };
   const addGoalHandler = () => {
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+    setCourseGoals(currentGoals => [...currentGoals, 
+      { key: Math.random().toString(), value: enteredGoal }]);
   };
 
   return (
-    <ScrollView>
       <View style={styles.screen}>
         <Text style={styles.carePlan}> Care Plan </Text>
         <View style={styles.inputContainer}>
@@ -25,15 +25,13 @@ export default function App() {
           />
           <Button title="ADD" onPress={addGoalHandler} />
         </View>
-        <ScrollView>
-          {courseGoals.map(goal => (
-            <View key={goal} style={styles.listItem}>
-              <Text>{goal}</Text>
+        <FlatList
+          data={courseGoals} renderItem={itemData => (
+            <View style={styles.listItem}>
+              <Text>{itemData.item.value}</Text>
             </View>
-          ))}
-        </ScrollView>
+          )} />
       </View>
-    </ScrollView>
   );
 }
 
