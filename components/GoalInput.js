305b7b4a1573210027c05e1113857ad2da5f14b1
nancyruya <nancyruya@gmail.com>
2020-04-11 18:00:36 -0400

    sister was scared and in need of me

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -25,8 +25,8 @@ const GoalInput = props => {
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        flex: 1, 
+        justifyContent: 'center',
         alignItems: 'center'
     },
     input: {
