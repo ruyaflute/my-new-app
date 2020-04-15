e10575f61eef5171284b61e31796980b1c86e844
nancyruya <nancyruya@gmail.com>
2020-04-15 17:19:03 -0400

    Registration screen done

--- components/Regform.js
+++ components/Regform.js
@@ -5,7 +5,22 @@ export default class Regform extends Component {
   render() {
   return (
     <View style={styles.regform}>
-      <Text> Texting </Text>
+      <Text style={styles.header}>Registration</Text>
+
+      <TextInput style={styles.textinput} placeholder="Your name" 
+      underlineColorAndroid={'transparent'} />
+
+      <TextInput style={styles.textinput} placeholder="Your email" 
+      underlineColorAndroid={'transparent'} />
+
+      <TextInput style={styles.textinput} placeholder="Your password" 
+      secureTextEntry={true}
+      underlineColorAndroid={'transparent'} />
+
+      <TouchableOpacity style={styles.button} >
+      <Text style={styles.btntext}>Sign up </Text>
+      </TouchableOpacity>
+
     </View>
     );
   }
@@ -18,5 +33,33 @@ const styles = StyleSheet.create({
     backgroundColor: '#36485f',
     paddingLeft: 60,
     paddingRight: 60,
+    alignSelf: 'stretch',
   },
+  header: {
+    fontSize: 24,
+    color: '#fff',
+    paddingBottom: 10,
+    marginBottom: 40,
+    borderBottomColor: '#199187',
+    borderBottomWidth: 1,
+  },
+  textinput: {
+    alignSelf: 'stretch',
+    height: 40,
+    marginBottom: 30,
+    color: '#fff',
+    borderBottomColor: '#f8f8f8',
+    borderBottomWidth: 1,
+  },
+  button: {
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#59cbbd',
+    marginTop: 30,
+  },
+  btntext: {
+    color: '#fff',
+    fontWeight: 'bold',
+  }
 });
