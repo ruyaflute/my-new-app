2dd85cf45778f9999351fc5669e0cf9309959473
nancyruya <nancyruya@gmail.com>
2020-04-17 15:23:12 -0400

    some maybe so-called backend shit.. for forgot password sake

--- components/ForgotPassword.js
+++ components/ForgotPassword.js
@@ -2,60 +2,82 @@ import React, { Component } from 'react';
 import { Text, View, KeyboardAvoidingView, StyleSheet, TextInput } from 'react-native';
 
 export default class ForgotPassword extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formValid: true,
+      loadingVisible: false,
+      validEmail: false,
+      emailAddress: '',
+    };
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.goToNextStep = this.goToNextStep.bind(this);
+  }
 
-        };
+  handleEmailChange(email) {
+    const emailCheckRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    this.setState({ emailAddress: email });
+
+    if (!this.state.validEmail) {
+      if (emailCheckRegex.test(email)) {
+        this.setState({ validEmail: true });
+      }
+    } else {
+      if (!emailCheckRegex.test(email)) {
+        this.setState({ validEmail: false });
+      }
     }
-    handleEmailChange(email) {
+  }
 
-    }
-    render() {
-        return (
-            <KeyboardAvoidingView style={styles.wrapper}
-                behavior="padding">
-                <View style={styles.form}>
-                    <Text style={styles.forgotPasswordHeading}>Forgot your password?</Text>
-                    <Text style={styles.forgotPasswordSubheading}> Enter your email to find your account: </Text>
+  goToNextStep() {
+    this.setState({ loadingVisible: true });
+  }
 
-                    <TextInput
-                        placeholder="EMAIL ADDRESS"
-                        style={styles.inputField}
-                    />
+  render() {
+    return (
+      <KeyboardAvoidingView style={styles.wrapper}
+      behavior="padding">
+      <View style={styles.form}>
+      <Text style={styles.forgotPasswordHeading}>Forgot your password?</Text>
+      <Text style={styles.forgotPasswordSubheading}> Enter your email to find your account: </Text>
+      
+       <TextInput 
+      placeholder="EMAIL ADDRESS"
+      style={styles.inputField}  
+      />
 
-                </View>
-            </KeyboardAvoidingView>
-        );
-    }
+      </View>
+      </KeyboardAvoidingView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    wrapper: {
-        display: 'flex',
-        flex: 1,
-        backgroundColor: '#36485f',
-    },
-    form: {
-        marginTop: 90,
-        paddingLeft: 20,
-        paddingRight: 20,
-        flex: 1,
-    },
-    forgotPasswordHeading: {
-        fontSize: 28,
-        color: '#fff',
-        fontWeight: '300',
-    },
-    forgotPasswordSubheading: {
-        color: '#fff',
-        fontWeight: '600',
-        fontSize: 15,
-        marginTop: 10,
-        marginBottom: 60,
-    },
-    inputField: {
-        marginBottom: 30,
-        textColor: '#fff'
-    }
+  wrapper: {
+    display: 'flex',
+    flex: 1,
+    backgroundColor: '#36485f',
+  },
+  form: {
+    marginTop: 90,
+    paddingLeft: 20,
+    paddingRight: 20,
+    flex: 1,
+  },
+  forgotPasswordHeading: {
+    fontSize: 28,
+    color: '#fff',
+    fontWeight: '300',
+  },
+  forgotPasswordSubheading: {
+    color: '#fff',
+    fontWeight: '600',
+    fontSize: 15,
+    marginTop: 10,
+    marginBottom: 60,
+  },
+  inputField: {
+    marginBottom: 30,
+    textColor: '#fff'
+  }
 });
--- components/Login.js
+++ components/Login.js
@@ -7,63 +7,83 @@ export default class Login extends Component {
     this.state = {
       formValid: true,
       validEmail: false,
-      emailAddress: '',
+      emailAddress:'',
       validPassword: false,
       loadingVisible: false,
     }
+    this.handleCloseNotification = this.handleCloseNotification.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+  }
 
+  handleCloseNotification() {
+    this.setState({formValid: true });
+  }
+
+  handleEmailChange(email) {
+    const emailCheckRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    this.setState({ emailAddress: email });
+
+    if (!this.state.validEmaill) {
+      if (emailCheckRegex.test(email)) {
+        this.setState({ validEmail: true });
+      } 
+    } else {
+      if (!emailCheckRegex.test(email)) {
+        this.setState({ validEmail: false });
+      }
+    }
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <StatusBar barStyle="light-content" />
+      <StatusBar barStyle="light-content" />
         <View style={styles.logoContainer}>
           <Image
             style={styles.logo}
             source={require('assets/snack-icon.png')}
           />
-          <Text style={styles.title}>
-            Welcome to CMCC
+          <Text style={styles.title}> 
+          Welcome to CMCC
           </Text>
-          <Text style={styles.subtitle}>
-            The Best Case Management and Care Coordination tool
+          <Text style={styles.subtitle}> 
+          The Best Case Management and Care Coordination tool
           </Text>
         </View>
+        
+        <KeyboardAvoidingView behavior="padding" style={styles.loginFormContainer}> 
+        <TextInput 
+        placeholder="username or email"
+        placeholderTextColor="rgba(255, 255, 255, 0.7)"
+        returnKeyType="next"
+        onSubmitEditing={() => this.passwordInput.focus()}
+        keyboardType="emaill-address"
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.input} />
+        <TextInput 
+        placeholder="password"
+        placeholderTextColor="rgba(255, 255, 255, 0.7)"
+        returnKeyType="enter"
+        secureTextEntry
+        style={styles.input} 
+        ref={(input) => this.passwordInput = input}/>
+        
+        <TouchableOpacity style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>LOGIN</Text>
+        </TouchableOpacity>
 
-        <KeyboardAvoidingView behavior="padding" style={styles.loginFormContainer}>
-          <TextInput
-            placeholder="username or email"
-            placeholderTextColor="rgba(255, 255, 255, 0.7)"
-            returnKeyType="next"
-            onSubmitEditing={() => this.passwordInput.focus()}
-            keyboardType="emaill-address"
-            autoCapitalize="none"
-            autoCorrect={false}
-            style={styles.input} />
-          <TextInput
-            placeholder="password"
-            placeholderTextColor="rgba(255, 255, 255, 0.7)"
-            returnKeyType="enter"
-            secureTextEntry
-            style={styles.input}
-            ref={(input) => this.passwordInput = input} />
-
-          <TouchableOpacity style={styles.buttonContainer}>
-            <Text style={styles.buttonText}>LOGIN</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.retrivePassword}>
-            <Text style={styles.title}>
-              Don't have an account? Sign up!
+        <TouchableOpacity style={styles.retrivePassword}> 
+          <Text style={styles.title}> 
+          Don't have an account? Sign up!
           </Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.retrivePassword}>
-            <Text style={styles.title}>
-              Dont't remember your password?
+        </TouchableOpacity>
+          
+          <TouchableOpacity style={styles.retrivePassword}> 
+          <Text style={styles.title}> 
+          Dont't remember your password?
           </Text>
-          </TouchableOpacity>
+        </TouchableOpacity>
 
         </KeyboardAvoidingView>
 
@@ -90,7 +110,7 @@ const styles = StyleSheet.create({
   },
   title: {
     color: '#FFF',
-    marginTop: 10,
+    marginTop: 10, 
     width: 160,
     textAlign: 'center',
     opacity: 0.9,
@@ -102,7 +122,7 @@ const styles = StyleSheet.create({
   },
   loginFormContainer: {
     padding: 20,
-  },
+  }, 
   input: {
     height: 40,
     backgroundColor: 'rgba(255, 255, 255, 0.2)',
