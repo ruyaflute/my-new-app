7756333106ad38db518f63c3118e39e7cd352ff5
nancyruya <nancyruya@gmail.com>
2020-04-23 17:14:00 -0400

    Nativation got messed up, and need more design

--- App.js
+++ App.js
@@ -1,18 +1,42 @@
+import 'react-native-gesture-handler';
 import React, { Component } from 'react';
-import { View, StyleSheet, Image, Text, TextInput, TouchableOpacity, KeyboardAvoidingView, StatusBar } from 'react-native';
-import Login from './screens/Login';
 
-const App = () => {
+//import Login from './screens/Login';
+import Clients from './screens/Clients';
+import Profile from './screens/Profile';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+const MaterialBottomTabs = createMaterialBottomTabNavigator();
+const MaterialTopTabs = createMaterialTopTabNavigator();
+
+export default class App extends Component {
+
+  render() {
+
+    createHomeStack = () =>
+      <Stack.Navigator>
+        <Stack.Screen name="Profile" component={Profile} />
+      </Stack.Navigator>
+
     return (
-      <Login />
+      <NavigationContainer>
+        <Drawer.Navigator>
+          <Drawer.Screen name="Home" children={createHomeStack} />
+          <Drawer.Screen name="Contacts" component={Clients} />
+          <Drawer.Screen name="Favorites" />
+          <Drawer.Screen name="Settings" />
+
+        </Drawer.Navigator>
+      </NavigationContainer>
     );
+  }
 }
 
-export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#36485f',
-  },
-});
